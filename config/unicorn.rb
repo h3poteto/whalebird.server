9a5f969811fb5666d4a77398ded207cceae0a5a8
h3poteto <h3.poteto@gmail.com>
2014-11-08 00:11:32 +0900

    add unicorn settings

---
+++ config/unicorn.rb
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# ワーカーの数
+worker_processes 2
+
+# ソケット経由で通信する
+listen File.expand_path('tmp/sockets/unicorn.sock', ENV['RAILS_ROOT'])
+
+# ログ
+stderr_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
+stdout_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
+
+# ダウンタイムなくす
+preload_app true
+
+timeout 45
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{ server.config[:pid] }.oldbin"
+  unless old_pid == server.pid
+    begin
+      # SIGTTOU だと worker_processes が多いときおかしい気がする
+      Process.kill :QUIT, File.read(old_pid).to_i
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
