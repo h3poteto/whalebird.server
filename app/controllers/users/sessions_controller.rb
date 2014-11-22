d9d0ac16b37fb81f2f0863cb3bb4cc7f6455d4fe
h3poteto <h3.poteto@gmail.com>
2014-11-23 01:07:42 +0900

    sign_in用のviewとtokenの作成

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,4 +2,11 @@ class Users::SessionsController < Devise::SessionsController
   skip_before_action :verify_authenticity_token, only: [:destroy], if: Proc.new{|app|
     request.format == :json
   }
+  before_action :check_application_key, only: [:new]
+
+
+  private
+    def check_application_key
+      redirect_to root_path if params[:whalebird].to_s != ENV["WHALEBIRD_APPLICATION_KEY"]
+    end
 end
