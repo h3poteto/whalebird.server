07bf09c64caab47f125542ba7b825400750dd18e
h3poteto <h3.poteto@gmail.com>
2015-02-19 11:04:05 +0900

    fixes requests spec files

--- spec/requests/statics_spec.rb
+++ spec/requests/statics_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Statics", :type => :request do
   describe "GET /statics" do
-    it "works! (now write some real specs)" do
+    it "works!" do
       get statics_path
       expect(response).to have_http_status(200)
     end
--- spec/requests/users/users_apis_spec.rb
+++ spec/requests/users/users_apis_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "Users::Apis", :type => :request do
   describe "GET /users_apis" do
-    it "works! (now write some real specs)" do
+    it "redirect to top" do
       get users_apis_path
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(302)
     end
   end
 end
