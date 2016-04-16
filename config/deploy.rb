a3c038778ec999a28c114a4f83e9cdc793c7fde6
h3poteto <h3.poteto@gmail.com>
2016-04-16 17:28:21 +0900

    capが使うrubyのバージョンを2.3に固定

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,7 +39,7 @@ set :keep_releases, 5
 
 set :rbenv_path, '~/.rbenv'
 set :rbenv_type, :system
-set :rbenv_ruby, '2.1.3'
+set :rbenv_ruby, '2.3.0'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all
