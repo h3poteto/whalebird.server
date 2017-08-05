0922e7a1a99c2667309bc2f0bb2a2be867331c02
h3poteto <h3.poteto@gmail.com>
2017-08-05 23:35:52 +0900

    refs #94 update rubocop

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 Sidekiq.configure_server do |config|
   config.redis = {
-    url: "redis://#{ENV["REDIS_HOST"]}:#{ENV["REDIS_PORT"]}",
+    url: "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}",
     namespace: "#{Rails.application.class.parent}_#{Rails.env}"
   }
   # for sidekiq-failures
@@ -9,7 +11,7 @@ end
 
 Sidekiq.configure_client do |config|
   config.redis = {
-    url: "redis://#{ENV["REDIS_HOST"]}:#{ENV["REDIS_PORT"]}",
+    url: "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}",
     namespace: "#{Rails.application.class.parent}_#{Rails.env}"
   }
 end
